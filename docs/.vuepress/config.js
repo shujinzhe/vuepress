04cd0340bc9a81ddadbd56bb355df2675ae47c88
shujinzhe <946636673@qq.com>
2019-10-13 01:58:09 +0800

    deploy_source

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	base: '/vuepress/',
 	title: '岳明亮的个人笔记',
-	description: 'shujinzhe的个人笔记',
+	description: '生活不止眼前的苟且，还有诗与远方',
 	head: [
-		['link', { rel: 'icon', href: '/img/logo.png' }]
+		['link', { rel: 'icon', href: '/img/logo.ico' }]
 	],
 	port: 8080,
 	dest: '../vuepress_deploy/dist',
