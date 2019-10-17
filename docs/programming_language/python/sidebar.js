c0b82bc1aa07d21b768faa13bb280c8a0c710feb
shujinzhe <946636673@qq.com>
2019-10-17 09:33:15 +0800

    deploy_source

--- docs/programming_language/python/sidebar.js
+++ docs/programming_language/python/sidebar.js
@@ -3,8 +3,7 @@ module.exports = [
 	  title:'Python入门',
 	  collapsable: true,
 	  children:[
-		'/programming_language/python/python_intro',
-		'/programming_language/python/python_intro2'
+		'/programming_language/python/python_first_glance'
 	  ]
 	}
 ]
