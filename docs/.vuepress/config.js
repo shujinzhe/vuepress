1d97318bf2e5b85a10d1a253af383a15dc208766
shujinzhe <946636673@qq.com>
2019-10-20 09:29:19 +0800

    deploy_source

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,13 +13,29 @@ module.exports = {
 		}
 	},
 	themeConfig: {
+        // 假定 GitHub。也可以是一个完整的 GitLab 网址
+        repo: 'shujinzhe/vuepress',
+        // 如果你的文档不在仓库的根部
+        docsDir: 'docs',
+        // 可选，默认为 master
+        docsBranch: 'master',
+        // 默认为 true，设置为 false 来禁用
+        editLinks: true,
+        // 显示上次更新字样
 		lastUpdated: '上次更新',
-		activeHeaderLinks: false, // 默认值：true
+        // 默认值：true
+		activeHeaderLinks: false,
+        // 展开所有子标题
 		displayAllHeaders: false,
+        // 导航栏配置
 		nav: require("./nav.js"),
+        // 侧边栏配置
 		sidebar: require("./sidebar.js"),
+        // 侧边栏深度，默认为1
 		sidebarDepth: 2,
+        // 搜索最大返回条数
 		searchMaxSuggestoins: 10,
+        // 编辑连接
 		editLinks: true,
 		editLinkText: '在GitHub上编辑此页！'
 	}
